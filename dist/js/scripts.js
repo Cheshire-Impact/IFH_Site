0fd7ae07428a8b515bf0752c04b11f9a34b762c3
maljenkins <53225525+maljenkins@users.noreply.github.com>
2020-06-29 08:45:43 -0400

    Major updates

---
+++ dist/js/scripts.js
@@ -0,0 +1,8 @@
+document.querySelector('video').playbackRate = .5;
+document.querySelector('video').play();
+
+
+$(document).ready(function () {
+    console.log("here");
+    $(".ul.navbar-nav::after").animate({ width: "500px" });
+});
