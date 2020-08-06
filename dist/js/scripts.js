d9150dec4de35047fb7322f39e4a290f13df186a
maljenkins <53225525+maljenkins@users.noreply.github.com>
2020-08-06 11:46:26 -0400

    8/5 Updates

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -2,7 +2,20 @@ document.querySelector('video').playbackRate = .5;
 document.querySelector('video').play();
 
 
-$(document).ready(function () {
-    console.log("here");
-    $(".ul.navbar-nav::after").animate({ width: "500px" });
-});
+// Resize Video Wrap
+
+
+
+
+window.addEventListener('resize', resizeVideo());
+
+function resizeVideo() {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    const videoWrap = document.querySelector(".fullscreen-video-wrap");
+
+    videoWrap.width = w;
+    videoWrap.height = w;
+}
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,25 @@ const nunjucksRender = require("gulp-nunjucks-render");
 const sass = require("gulp-sass");
 const prefix = require('gulp-autoprefixer');
 const data = require("gulp-data");
+const imagemin = require('gulp-imagemin');
 const prettier = require("gulp-prettier");
+const plumber = require("gulp-plumber");
 const browserSync = require("browser-sync");
 const server = browserSync.create();
+const bless = require('gulp-bless');
 
 
 const paths = {
   styles: {
-    src: 'src/scss/*.scss',
+    src: 'src/scss/**/*.scss',
     dest: 'dist/css/'
   },
   html: {
     src: 'src/templates/**/*'
+  },
+  image: {
+    src: 'src/images/**/*',
+    dest: 'dist/images/'
   }
 };
 
@@ -36,6 +43,7 @@ function serve(done) {
 
 function nunjucks(done) {
   gulp.src("./src/templates/*.html")
+    .pipe(plumber())
     .pipe(
       data(function () {
         return require("./src/data/global.json");
@@ -52,36 +60,37 @@ function nunjucks(done) {
 
 function style(done) {
   gulp.src(paths.styles.src)
+    .pipe(plumber())
     .pipe(sass())
-    .pipe(prefix())
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(prefix({ "grid": "autoplace" }))
+    .pipe(bless())
+    .pipe(gulp.dest(paths.styles.dest));
   done();
 }
 
 
-// function pretty(done) {
-//   gulp.src("./dist/*.html")
-//     .pipe(prettier({ singleQuote: true }))
-//     .pipe(gulp.dest("./dist/"));
-//   done();
-// }
 
-// function styleWatch() {
-//   gulp.watch("./src/scss/*.scss", style);
-// }
+function compress(done) {
+  gulp.src(paths.image.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.image.dest));
+  done();
+}
+
+
+function pretty(done) {
+  gulp.src("./dist/*.html")
+    .pipe(plumber())
+    .pipe(prettier({ singleQuote: true }))
+    .pipe(gulp.dest("./dist/"));
+  done();
+}
+
 
 
 const watch = () => gulp.watch([paths.styles.src, paths.html.src], gulp.series(nunjucks, style, reload));
 
 const dev = gulp.series(nunjucks, style, serve, watch);
+const deploy = gulp.series(pretty, compress);
 exports.default = dev;
-
-
-
-// gulp.task("default", gulp.series(nunjucks, pretty, style));
-// gulp.task("watch", styleWatch);
-// gulp.task("serve", serve);
-
-// const build = gulp.series(nunjucks, pretty, styles);
-
-// exports.default = build;
+exports.deploy = deploy;
